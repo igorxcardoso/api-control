f592cf744a6d257958c5f5eb18d60445f888c313
Igor Cardoso <iygorsilva@gmail.com>
2023-03-16 21:26:43 -0300

    project start

---
+++ src/main/java/com/api/apicontrol/ApiControlApplication.java
@@ -0,0 +1,13 @@
+package com.api.apicontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/apicontrol/ApiControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.apicontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
