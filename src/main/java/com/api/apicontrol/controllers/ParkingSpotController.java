fb83d1fda5c55da276946fc370d67f8375b934d7
Igor Cardoso <iygorsilva@gmail.com>
2023-03-17 00:04:18 -0300

    POST method creation

--- src/main/java/com/api/apicontrol/controllers/ParkingSpotController.java
+++ src/main/java/com/api/apicontrol/controllers/ParkingSpotController.java
@@ -1,18 +1,43 @@
 package com.api.apicontrol.controllers;
 
+import com.api.apicontrol.dtos.ParkingSpotDto;
+import com.api.apicontrol.models.ParkingSpotModel;
 import com.api.apicontrol.services.ParkingSpotService;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
-@RequestMapping("/control")
+@RequestMapping("/control") // A URI foi definida a nível de classe
 public class ParkingSpotController {
     final ParkingSpotService parkingSpotService;
 
     // Construtor. Um ponto de injeção para ParkingSpotService
     public ParkingSpotController(ParkingSpotService parkingSpotService) {
         this.parkingSpotService = parkingSpotService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> createParkingSpot(@RequestBody @Valid ParkingSpotDto parkingSpotDto) {
+        // O @RequestBody é para receber a requisição como json
+        // O @Valid para fazer as validações dos DTOs. Se tiver algum erro com os dados na requisição, nem entra no método.
+
+        var parkingSpotModel = new ParkingSpotModel();
+
+        // Conversão de DTO para model, pois estamos recendo um DTO no método, mas persistimos um model.
+        BeanUtils.copyProperties(parkingSpotDto, parkingSpotModel);
+
+        // Faz o set da data de registro
+        parkingSpotModel.setRegistrationDate(LocalDateTime.now(ZoneId.of("UTC")));
+
+        // ResponseEntity para construir a resposta
+        return ResponseEntity.status(HttpStatus.CREATED).body(parkingSpotService.save(parkingSpotModel));
     }
 }
