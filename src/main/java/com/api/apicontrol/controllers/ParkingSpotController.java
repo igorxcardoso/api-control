70b98cd19e02a6035469553c2f225b8860b59e65
Igor Cardoso <iygorsilva@gmail.com>
2023-04-23 15:38:48 -0300

    add pagination to indexParkingSpots enpoint

--- src/main/java/com/api/apicontrol/controllers/ParkingSpotController.java
+++ src/main/java/com/api/apicontrol/controllers/ParkingSpotController.java
@@ -5,6 +5,10 @@ import com.api.apicontrol.models.ParkingSpotModel;
 import com.api.apicontrol.services.ParkingSpotService;
 
 import org.springframework.beans.BeanUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -57,8 +61,8 @@ public class ParkingSpotController {
 
     // GET /control
     @GetMapping
-    public ResponseEntity<List<ParkingSpotModel>> indexParkingSpots() {
-        return ResponseEntity.status(HttpStatus.OK).body(parkingSpotService.findAll());
+    public ResponseEntity<Page<ParkingSpotModel>> indexParkingSpots(@PageableDefault(page = 0, size = 10, sort= "id", direction = Sort.Direction.ASC) Pageable pageable) {
+        return ResponseEntity.status(HttpStatus.OK).body(parkingSpotService.findAll(pageable));
     }
 
     // GET  /control/{id}
