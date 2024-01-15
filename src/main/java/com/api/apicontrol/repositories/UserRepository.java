3c6865b6b0bf749ecbb461dbd30dc15ff2356624
Igor Cardoso <iygorsilva@gmail.com>
2024-01-15 07:49:05 -0300

    add security configurations and user-related classes (not authenticating yet)

---
+++ src/main/java/com/api/apicontrol/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.api.apicontrol.repositories;
+
+import com.api.apicontrol.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<User, String> {
+    UserDetails findByLogin(String login);
+}
